cd35babbf6cf39499b4b06b824934c385c7c96b1
tr00per92 <zvezdo@mail.bg>
2015-01-03 19:26:07 +0200

    Addition to the last commit

---
+++ BattleField/BattleFieldGame.cs
@@ -0,0 +1,56 @@
+﻿namespace BattleField
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class BattleFieldGame
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the \"Battle Field\" game.");
+            Console.Write("Enter the size of the battle field between 1 and 10: ");
+            int battleFieldSize;
+            if (!int.TryParse(Console.ReadLine(), out battleFieldSize))
+            {
+                throw new ArgumentException("You must enter a valid integer.");
+            }
+
+            BattleField battleField = new BattleField(battleFieldSize);
+            Console.WriteLine(battleField);
+            while (true)
+            {
+                Console.Write("Please enter coordinates: ");
+                string input = Console.ReadLine();
+                if (input == null)
+                {
+                    throw new ArgumentException("You must enter coordinates.");
+                }
+
+                int row, col;
+                string[] inputCoordinates = Regex.Split(input, "\\s+");
+                if (inputCoordinates.Length != 2 ||
+                    !int.TryParse(inputCoordinates[0], out row) ||
+                    !int.TryParse(inputCoordinates[1], out col))
+                {
+                    throw new ArgumentException("You must enter at least two integer coordinates.");
+                }
+
+                try
+                {
+                    battleField.ProccessMove(row, col);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine(battleField);
+                if (battleField.AllMinesAreDetonated)
+                {
+                    Console.WriteLine("Game over. Detonated mines: {0}", battleField.DetonatedMinesCount);
+                    break;
+                }
+            }
+        }
+    }
+}
